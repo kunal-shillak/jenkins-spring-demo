2a058346bd71afe287c796aeb78b1182c9f098ec
kunal-shillak <kunal.shillak1@gmail.com>
2026-09-16 23:09:44 +0530

    Add calculator API

---
+++ src/main/java/com/kunal/jenkinsSpringDemo/JenkinsSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.jenkinsSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunal/jenkinsSpringDemo/JenkinsSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.jenkinsSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
